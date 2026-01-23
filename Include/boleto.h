1a2cd2961b4df3a5f6e9c1b4dc5d1abaf8c3f042
fabricio-RH <fabricioroque369@gmail.com>
2026-01-22 23:18:55 -0500

    B

--- Include/boleto.h
+++ Include/boleto.h
@@ -1,10 +1,35 @@
 #ifndef BOLETO_H
 #define BOLETO_H
-#include <string>
-#include <iostream>
-using namespace std;
 
 class Boleto {
-private:
+    private:
+        int cod_boleto;
+        float precio;
+        char codigo_vuelo[20];
+        char asiento[10];
+        char tipo[20];
+        char dni_pasajero[15];
 
-public:
+    public:
+        Boleto();
+        Boleto(int cod, float precio, char Vuelo[20], char Asiento[10], char Tipo[20], char Dni[15]);
+
+        void SetCodigo(int cod);
+        void SetBoleto(float precio, char Vuelo[20], char Asiento[10], char Tipo[20], char Dni[15]);
+        void SetBoleto(Boleto boleto); 
+        void SetPrecio(float Precio);
+
+        int GetCodigo();
+        float GetPrecio();
+        char* GetCodigoVuelo();
+        char* GetAsiento();
+        char* GetTipo();
+        char* GetDniPasajero();
+
+        void Mostrar();
+        void MostrarConsulta();
+        
+        ~Boleto();
+};
+
+#endif
